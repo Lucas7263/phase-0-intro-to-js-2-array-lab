5c6e399d133d42fd067683a430390541b23b2763
Lucas <lucaswoodyard@yahoo.com>
2022-07-26 17:47:41 -0500

    commit tree 150390f8447a0c73aefc9674c7baa8f14ec60c44

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Write your solution here!
+// Write your solution here
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+
+function arrayMethods() {
+  
+  
+    cats.push("Milo", "Otis", "Garfield");
+  };
+
+  function destructivelyAppendCat() {
+    cats.push("Ralph");
+  };
+
+  function destructivelyPrependCat() {
+    cats.unshift("Bob");
+  };
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  };
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  };
+
+  function appendCat(newCat) {
+    const cats2 = [...cats, newCat]; 
+    return cats2; 
+    }
+
+    function prependCat(newCat) {
+        const cats2 = [newCat, ...cats];
+        return cats2;
+    }
+
+    function removeLastCat() {
+        const cats2 = [...cats]
+        cats2.pop();
+        return cats2; 
+    }
+
+    function removeFirstCat() {
+        const cats2 = [...cats]
+        cats2.shift();
+        return cats2; 
+    }
+
+
+
+   
+    
